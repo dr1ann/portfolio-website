8d680e7dabd8eb55f2659e17e5118f215eed1875
James <jamesdenoy56@gmail.com>
2023-06-25 21:47:05 +0800

    next config js update

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    output: {
+        // Set the export target to 'static' for static HTML export
+        export: 'static',
+      }
+}
 
 module.exports = nextConfig
