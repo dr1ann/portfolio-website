ec5b4fc4db96021566b851d78240c80c57b76f4d
James <jamesdenoy56@gmail.com>
2023-06-26 13:44:47 +0800

    added image optimization in next config js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
- 
-}
-
-module.exports = nextConfig
+    output: 'export',
+    images: {
+      unoptimized: true,
+    },
+  };
+  
+  module.exports = nextConfig;
+  
